8c0cd9c23fc5a4bb953bba674a0ccb12df620d36
ykimura517 <y.kimura199412@gmail.com>
2019-05-19 22:23:07 +0900

    display image and prepare dict containing user info

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
@@ -6,9 +7,46 @@ import * as serviceWorker from './serviceWorker';
 
 // ReactDOM.render(<App />, document.getElementById('root'));
 ReactDOM.render(
-  <h1>Hello world!</h1>,
+  <h1>Like or Nope ?</h1>,
   document.getElementById('root')
 );
+ReactDOM.render(
+<img src={`${process.env.PUBLIC_URL}/img/3.png`} alt="Logo" class="user-image" />,
+  document.getElementById('user-image-container')
+);
+
+
+
+var Users = [
+    {
+      name: 'bangya',
+      age: 25,
+      img: `${process.env.PUBLIC_URL}/img/1.png`
+    },
+    {
+      name: 'ghost',
+      age: 20,
+      img: `${process.env.PUBLIC_URL}/img/2.png`
+    },
+    {
+      name: 'Harajuku',
+      age: 18,
+      img: `${process.env.PUBLIC_URL}/img/3.png`
+    }
+  ];
+
+
+
+
+
+
+
+
+
+
+
+
+
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
